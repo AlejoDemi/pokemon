a5898b2636a6ecb868b7a7cd467200c006dcc5f7
alejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-06 16:53:36 -0300

    creation

---
+++ src/components/PokemonTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokemonTable = () => {
+    return (
+        <div>
+            <h1>Pokemon list goes here</h1>
+        </div>
+    );
+};
+
+export default PokemonTable;
